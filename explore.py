e905cf0c0456243da3835bbce62eb68c68b6effa
Larry Holmes <larry.b.holmes.2@gmail.com>
2022-09-01 16:40:46 -0500

    Updating my explore py file and explore exercises

--- explore.py
+++ explore.py
@@ -4,12 +4,40 @@ import numpy as np
 # viz imports
 import seaborn as sns
 import matplotlib.pyplot as plt
+import plotly.express as px
 
-def plot_variable_pairs(train):
-    sns.pairplot(data=train,
+def plot_variable_pairs(df):
+    samp_df = df.sample(frac=.083)
+    sns.pairplot(data=samp_df,
             corner=True,
             kind='reg')
     plt.show()
 
-# def plot_categorical_and_continuous_vars(train, col_selection):
-    # for 
+def dtypes_to_list(df):
+    num_type_list , cat_type_list = [], []
+
+    for column in df:
+        col_type = df[column].dtype
+        if col_type == 'object':
+            cat_type_list.append(column)
+        if np.issubdtype(df[column], np.number) and \
+             ((df[column].max() + 1) / df[column].nunique())  == 1 :
+            cat_type_list.append(column)
+        if np.issubdtype(df[column], np.number) and \
+            ((df[column].max() + 1) / df[column].nunique()) != 1 :
+            num_type_list.append(column)
+    return num_type_list, cat_type_list  
+
+def plot_categorical_and_continuous_vars(df, num_type_list, cat_type_list):
+    
+    for column in df:
+        if column.isin(num_type_list):
+            sns.lmplot(data=df, x=column, y=df.tax_value)
+            plt.title(f'{column}')
+            plt.xlabel(f'{column}')
+            plt.ylabel('y = tax value')
+        elif column.isin(cat_type_list):
+            sns.barplot(data=df, x=column, y=df.tax_value)
+            plt.title(f'{column}')
+            plt.xlabel(f'{column}')
+            plt.ylabel('y = tax value')
