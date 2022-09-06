297dd0fdd15dfd6ae50bab0d82a5bd5d390452ea
Larry Holmes <larry.b.holmes.2@gmail.com>
2022-09-06 14:02:13 -0500

    adding modeling exercises to github and updating wrangle and feature engineering files

--- wrangle.py
+++ wrangle.py
@@ -73,11 +73,52 @@ def remove_outliers(df, k, col_list):
         
     return df
 
-def my_train_test_split(df):
-    
-    train, test = train_test_split(df, test_size=.2, random_state=123)
-    train, validate = train_test_split(train, test_size=.3, random_state=123)
-    
+def train_validate_test(df, target):
+    """
+    this function takes in a dataframe and splits it into 3 samples,
+    a test, which is 20% of the entire dataframe,
+    a validate, which is 24% of the entire dataframe,
+    and a train, which is 56% of the entire dataframe.
+    It then splits each of the 3 samples into a dataframe with independent variables
+    and a series with the dependent, or target variable.
+    The function returns 3 dataframes and 3 series:
+    X_train (df) & y_train (series), X_validate & y_validate, X_test & y_test.
+    """
+    # split df into test (20%) and train_validate (80%)
+    train_validate, test = train_test_split(df, test_size=0.2, random_state=123)
+
+    # split train_validate off into train (70% of 80% = 56%) and validate (30% of 80% = 24%)
+    train, validate = train_test_split(train_validate, test_size=0.3, random_state=123)
+
+    # split train into X (dataframe, drop target) & y (series, keep target only)
+    X_train = train.drop(columns=[target])
+    y_train = train[target]
+
+    # split validate into X (dataframe, drop target) & y (series, keep target only)
+    X_validate = validate.drop(columns=[target])
+    y_validate = validate[target]
+
+    # split test into X (dataframe, drop target) & y (series, keep target only)
+    X_test = test.drop(columns=[target])
+    y_test = test[target]
+
+    return X_train, y_train, X_validate, y_validate, X_test, y_test
+
+def split_continuous(df):
+    """
+    Takes in a df
+    Returns train, validate, and test DataFrames
+    """
+    # Create train_validate and test datasets
+    train_validate, test = train_test_split(df, test_size=0.2, random_state=123)
+    # Create train and validate datsets
+    train, validate = train_test_split(train_validate, test_size=0.3, random_state=123)
+
+    # Take a look at your split datasets
+
+    print(f"train -> {train.shape}")
+    print(f"validate -> {validate.shape}")
+    print(f"test -> {test.shape}")
     return train, validate, test
 
 def acquire_zillow():
